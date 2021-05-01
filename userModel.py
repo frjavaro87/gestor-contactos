7af1774957792c72e7c28fccde43e6c4974e9314
“Javier <frjavaro87@gmail.com>
2021-05-01 18:45:58 -0500

    problemaconelmain

--- userModel.py
+++ userModel.py
@@ -1,7 +1,7 @@
 #usuarios
 import db_manager
 
-DATABASE_NAME = "ContactManager.db" 
+DATABASE_NAME = "usuarios.db" 
 
 dbmngr = db_manager.DbManager(DATABASE_NAME)
 
@@ -20,14 +20,14 @@ class users:
         self.Id = int(input("Ingrese el ID: "))
         self.Pass = input("Ingrese el Pass: ")
         self.Nombre = input("Ingrese el Nombre: ")
-        self.Apellido = int(input("Ingrese el Apellido: "))
+        self.Apellido = (input("Ingrese el Apellido: "))
         self.Perfil = input("Ingrese el Perfil: ")
         self.Area = input("Ingrese el Area: ")
         
         dbmngr.connectDB()
         conexion = dbmngr.conn
         cursor = dbmngr.conn.cursor()
-        cursor.execute("Insert into contacts (Id, Pass, Nombre, Apellido, Perfil, Area) values (?,?,?,?,?,?)",
+        cursor.execute("Insert into usersTable (id, password, nombre, apellido, perfil, area) values (?,?,?,?,?,?)",
                         (self.Id, self.Pass, self.Nombre, self.Apellido, self.Perfil, self.Area))
 
         conexion.commit()
@@ -37,7 +37,7 @@ class users:
         print(f'comunicacion realizada, user {self.id} creado excelente')
 
 def main():
-    contacto = Usuario()
+    contacto = users()
     contacto.createUser()
 
 if __name__ == '__main__':
